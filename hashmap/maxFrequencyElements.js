ed6fb8a3c64f77f27c44001054414c7dc7d9fed9
aman <amangodara5686@gmail.com>
2026-05-19 13:07:54 +0530

    solved maxFrequencyElements

---
+++ hashmap/maxFrequencyElements.js
@@ -0,0 +1,47 @@
+/*
+You are given an array nums consisting of positive integers.
+
+Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+
+The frequency of an element is the number of occurrences of that element in the array.
+
+Example 1:
+
+Input: nums = [1,2,2,3,1,4]
+Output: 4
+Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+So the number of elements in the array with maximum frequency is 4.
+Example 2:
+
+Input: nums = [1,2,3,4,5]
+Output: 5
+Explanation: All elements of the array have a frequency of 1 which is the maximum.
+So the number of elements in the array with maximum frequency is 5.
+
+*/
+
+const maxFrequencyElements = function (nums) {
+  let maxFreq = 0;
+  let maxFreqCount = 0;
+
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+
+    const updatedCount = (map.get(num) || 0) + 1;
+    map.set(num, updatedCount);
+
+    if (updatedCount > maxFreq) {
+      maxFreq = updatedCount;
+      maxFreqCount = updatedCount;
+    } else if (updatedCount === maxFreq) {
+      maxFreqCount += updatedCount;
+    }
+  }
+
+  return maxFreqCount;
+};
+
+console.log(maxFrequencyElements([1, 2, 2, 3, 1, 4])); // 4
+console.log(maxFrequencyElements([1, 2, 3, 4, 5])); // 5
